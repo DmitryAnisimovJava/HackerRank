9207f02fb55f9f66caf6d298ecdc0ee275855f94
DmitryAnisimovJava <130083398+DmitryAnisimovJava@users.noreply.github.com>
2023-09-01 14:30:14 +0300

    Add DateAndTime

--- Java/basics/DateAndTime.java
+++ Java/basics/DateAndTime.java
@@ -6,19 +6,26 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.time.DayOfWeek;
+import java.util.Calendar;
 
 public class DateAndTime {
 	public static void main(String[] args) throws IOException {
 		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(new File("C:\\Users\\Anisi\\Desktop\\output.txt")));
 
-        String firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "");
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
 
-        
+        int month = Integer.parseInt(firstMultipleInput[0]);
 
+        int day = Integer.parseInt(firstMultipleInput[1]);
+
+        int year = Integer.parseInt(firstMultipleInput[2]);
+
+        String res = Result.findDay(month, day, year);
         
-        System.out.println(firstMultipleInput);
-        bufferedWriter.write(firstMultipleInput);
+        bufferedWriter.write(res);
+        System.out.println(res);
         bufferedWriter.newLine();
 
         bufferedReader.close();
@@ -28,7 +35,19 @@ public class DateAndTime {
 
 class Result {
 	
-	//public static String findDay(int month, int day, int year) {
-		
-	//}
+	public static String findDay(int month, int day, int year) {
+		Calendar myCalendar = Calendar.getInstance();
+		myCalendar.set(year, month - 1, day);
+		String dayInWeek = switch (myCalendar.get(Calendar.DAY_OF_WEEK)) {
+		case 1 -> "SUNDAY";
+		case 2 -> "MONDAY";
+		case 3 -> "TUESDAY";
+		case 4 -> "WEDNESDAY";
+		case 5 -> "THURSDAY";
+		case 6 -> "FRIDAY";
+		case 7 -> "SATURDAY";
+		default -> "Mistake in Date Number";
+		};
+		return dayInWeek;
+	}
 }
