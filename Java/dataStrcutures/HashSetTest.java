f4a47fefa5ee88ff31dcd7eb9eb9ce6119dd5eb0
DmitryAnisimovJava <130083398+DmitryAnisimovJava@users.noreply.github.com>
2023-09-11 15:26:04 +0300

    Add hashSet second time.

--- Java/dataStrcutures/HashSetTest.java
+++ Java/dataStrcutures/HashSetTest.java
@@ -1,10 +1,31 @@
 package dataStrcutures;
 
+import static java.lang.Math.rint;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class HashSetTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        String [] pair_left = new String[t];
+        String [] pair_right = new String[t];
+        
+        for (int i = 0; i < t; i++) {
+            pair_left[i] = s.next();
+            pair_right[i] = s.next();
+        }
+        int result = 0;
+        Set<String> mySet = new HashSet<>();
+        for (int i = 0; i < pair_right.length; i++) {
+			if (!mySet.contains(pair_left[i]+ " "+pair_right[i])) {
+				mySet.add(pair_left[i]+ " "+pair_right[i]);
+				result++;
+			}
+			System.out.println(result);
+		}
+   }
 }
