04ea48f66f555ead4301c71a14c8020edf0a1229
DmitryAnisimovJava <130083398+DmitryAnisimovJava@users.noreply.github.com>
2023-09-13 16:45:30 +0300

    Add final version of CanYouAccess challenge.

--- Java/advanced/CanYouAccessChallenge.java
+++ Java/advanced/CanYouAccessChallenge.java
@@ -1,10 +1,34 @@
 package advanced;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
 public class CanYouAccessChallenge {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) {	
 
-	}
-
-}
+		try{
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			int num = Integer.parseInt(br.readLine().trim());
+			Object o;// Must be used to hold the reference of the instance of the class Solution.Inner.Private
+			o = new Inner().new Private();
+			Inner.Private obj = new Inner().new Private();
+			System.out.println(num + " is " + obj.powerof2(num));
+			System.out.println("An instance of class: " + o.getClass().getCanonicalName() + " has been created");
+		
+		}//end of try
+		
+		catch (Exception e) {
+			System.out.println("Unsuccessful Termination!!");
+		}
+	}//end of main
+	static class Inner{
+		private class Private{
+			private String powerof2(int num){
+				return ((num & num - 1) == 0)?"power of 2":"not a power of 2";
+			}
+		}
+	}//end of Inner
+	
+}//end of Solution
