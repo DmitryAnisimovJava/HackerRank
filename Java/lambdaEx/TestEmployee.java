0ff9091d85feea50362f3bbbc5140cb80bd0f037
DmitryAnisimovJava <DmitryAnisimovJava@users.noreply.github.com>
2023-11-01 10:35:53 +0300

    Same exercise complete with Stream API.

--- Java/lambdaEx/TestEmployee.java
+++ Java/lambdaEx/TestEmployee.java
@@ -3,7 +3,9 @@ package lambdaEx;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 class TestEmployee {
 
@@ -27,5 +29,11 @@ class TestEmployee {
 				new Employee("Eva", "Bosser", new BigDecimal(20))));
 		TestEmployee tester = new TestEmployee();
 		tester.workerFilter(list, em -> em.getSalary() > 100);
+
+		List<Employee> exer1 = list.stream().filter(em -> em.getSalary() > 500).collect(Collectors.toList());
+		int exer2 = list.stream()
+				.max((em1, em2) -> Integer.valueOf(em1.getSalary()).compareTo(Integer.valueOf(em2.getSalary()))).get()
+				.getSalary();
+		System.out.println(exer2);
 	}
 }
