ff4640fe3cfcd67a310929091c24794a10653f95
DmitryAnisimovJava <DmitryAnisimovJava@users.noreply.github.com>
2023-11-06 14:49:37 +0300

    Result for 4 Lambda Calculus expression reductions.

---
+++ Java/functionalProgramming/LambdaCalculus.java
@@ -0,0 +1,29 @@
+package functionalProgramming;
+
+class LambdaCalculus {
+
+	public static void main(String[] args) {
+//		Task1
+//		((λx.(x y))(λz.z)
+//		(((λz.z) y))
+//		y
+
+//		Task2
+//		((λx.((λy.(x y))x))(λz.w))
+//		((((λy.((λz.w) y)) (λz.w))))
+//		(λy.((λz.w) y)) (λz.w)
+//		(λz.w) (λz.w)         z = (λz.w)  
+//		w 
+
+//		Task3
+//		((λx.(x x))(λx.(x x))) 
+//		((λx.(x x))(λx.(x x))) infinite 
+
+//		Task4
+//		λg.((λf.((λx.(f (x x)))(λx.(f (x x))))) g) 
+//		λg.(((λx.(g (x x)))(λx.(g (x x))))) 
+//		λg.((((g ((λx.(g (x x))) (λx.(g (x x)))))))) infinite 
+
+	}
+
+}
